5b83d98bffd50713ca5c9e8fd055f595893c0b15
kirdape64-create <kirdape64@gmail.com>
2026-09-12 14:45:41 -0700

    Steps 5 & 6 Complete

--- w6a/app.js
+++ w6a/app.js
@@ -17,6 +17,7 @@ const handleOrderSubmit = function (event) {
         timestamp: new Date().toISOString(),
     };
     orders.push(newOrder);
+    orderStorage.saveOrders(orders);
     console.log(orders);
     resultsDisplay.displayResults(newOrder);
 };
