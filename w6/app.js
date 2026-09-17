9ee5e608e3881b79bd81e506a0232ec91c76258c
kirdape64-create <kirdape64@gmail.com>
2026-09-17 10:47:27 -0700

    Step 3 Complete

--- w6/app.js
+++ w6/app.js
@@ -1,8 +1,9 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
+import * as orderList from './order-list.js';
 import * as orderForm from './order-handler.js';
 import * as priceCalculator from './price-calculator.js';
-// import * as resultsDisplay from './results-display.js';
 import * as orderStorage from './order-storage.js';
+
 
 const orderFormElement = document.getElementById('order-form');
 const orders = [];
@@ -18,15 +19,18 @@ const handleOrderSubmit = function (event) {
     };
     orders.push(newOrder);
     orderStorage.saveOrders(orders);
+    orderList.renderOrders(orders);
     console.log(orders);
     // resultsDisplay.displayResults(newOrder);
 };
 
 const init = function() {
     const loadedOrders = orderStorage.loadOrders();
+    
        if (loadedOrders.length > 0) {
         orders.push(...loadedOrders);
-        console.log("orders loaded from localStorage");
+        // Render the full list instead of just the last one
+        orderList.renderOrders(orders);
     } else {
         console.log("No orders found in localStorage starting fresh");
     }
